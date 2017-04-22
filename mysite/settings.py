1621884b129a3c6bf0fbdbac594649894df76baa
megmania <oh.youngmin@gmail.com>
2017-04-23 00:47:45 +0900

    My Django Girls app, settings.py host 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f*#d81wazoc-)a%3@m5n=x2@$te8^zl+09ns=(!q^d4n1t&3^j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['megmania.pythonanywhere.com']
 
 
 # Application definition
